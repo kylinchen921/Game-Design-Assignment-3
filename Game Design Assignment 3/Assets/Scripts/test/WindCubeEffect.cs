98da2efc0fb970200f91dc9dcdc8c36a04b9e9e6
kylinchen921 <foavin01@qq.com>
2015-05-15 16:30:01 +1000

    Level2 code1
    add some c# code and change level 2 cube

---
+++ Game Design Assignment 3/Assets/Scripts/test/WindCubeEffect.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WindCubeEffect : MonoBehaviour {
+
+	private Vector3 v3;
+	[SerializeField]
+	public float speed;
+	public float direction;
+
+
+	// Use this for initialization
+	void Start () {
+		if (direction == 1) {
+			v3 = Vector3.left;
+		} else if (direction == 2) {
+			v3 = Vector3.right;
+		} else if (direction == 3) {
+			v3 = Vector3.forward;
+		} else {
+			v3 = Vector3.back;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {}
+
+	void OnCollisionStay(Collision collisionInfo) {
+
+		GameObject go = collisionInfo.gameObject;
+		if (go.CompareTag ("Player")) {
+			go.transform.Translate (v3 * Time.deltaTime * speed, Space.World);
+		}
+
+	}
+}
